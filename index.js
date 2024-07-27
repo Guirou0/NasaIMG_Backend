33f72b48234e148c74d28d8cd5f818271b9e6ed7
Guirou0 <guirou112@gmail.com>
2024-07-27 17:47:16 -0300

    Fixes 2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const axios = require('axios');
 const path = require('path');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'dist')))
